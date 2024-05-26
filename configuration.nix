3521ef7f18198f57cb9e9df390a1dcb3c21aca4a
Roger Walz <oscar0228@gmail.com>
2024-05-26 10:37:56 -0400

    fixed syntax errors

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,7 @@
   environment.systemPackages = with pkgs; [
     micro # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+     kate
      git
      micro
      bitwarden
--- rogerw.nix
+++ rogerw.nix
@@ -1,4 +1,4 @@
- {pkg, ..}
+ {pkg, ... }:
  {
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -6,10 +6,6 @@
     isNormalUser = true;
     description = "Roger Walz";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-#       thunderbird
-        kate
-    ];
     openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxpo1SH1rlnkGKcdBkfguJww+B6JiLvMH35nUBTlp06 rogerw@saturn"
     ];
